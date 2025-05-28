2fc5b311337c189ba36bdfc1465cd153ff4d5009
Harish180830 <22btad022@kahedu.edu.in>
2025-05-28 06:39:09 +0000

    first-commit

---
+++ client/src/pages/dev.jsx
@@ -0,0 +1,18 @@
+export default function Dev() {
+  return (
+    <div id="d">
+      <div id="header">
+          <h1>Header</h1>
+      </div>
+      
+      <div id="section">
+      <h1>Section</h1>
+      </div>
+
+      <div id="footer">
+      <h1>Footer</h1>
+      </div>
+
+    </div>                           
+  );
+}
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   server:{
-    allowedHosts:["5173-akash1816-cms-project-ynf3kf8r7p.app.codeanywhere.com"]
+    allowedHosts:["5173-akash1816-cms-project-4mnbr6d2u8.app.codeanywhere.com"]
   },
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 })
