828984686e4bc421b17035ca6c009016cdd8fe0a
Akash1816 <22btad004@kahedu.edu.in>
2025-05-30 09:44:26 +0000

    routes updated

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,16 +1,17 @@
 import {Route, Routes} from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Dev from './pages/Dev';
-import Login from './pages/Login';
-import Logout from './pages/Logout';
-import Register from './pages/Register';
+import BasicLayouts from '../layouts/BasicLayouts';
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Dev from '../pages/Dev';
+import Login from '../pages/Login';
+import Logout from '../pages/Logout';
+import Register from '../pages/Register';
 
 export default function AppRoutes() {
     return (
         <Routes>
             <Route path="/"
-                element={<BasicLayout/>}>
+                element={<BasicLayouts/>}>
                 <Route index
                     element={<Home/>}/>
                 <Route path="/About"
