c75244eb210b86c4e90f321a47cd164ea36e13c4
Akash1816 <22btad004@kahedu.edu.in>
2025-05-31 05:11:44 +0000

    hosting

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server:{
-    allowedHosts:["5173-akash1816-cms-project-ynf3kf8r7p.app.codeanywhere.com"]
-  },
-  plugins: [react(),tailwindcss()],
+    server: {
+        allowedHosts: ["5173-akash1816-cms-project-ynf3kf8r7p.app.codeanywhere.com"]
+    },
+    plugins: [
+        react(), tailwindcss()
+    ],
+    build: {
+        rollupOptions: {
+            external: ['sweetalert2']
+        }
+    }
 })
